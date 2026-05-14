deeb6f72111d650390b56005e22c34ca1bcc7f19
Gabriel Galeano <gabrielgaleano4@hotmail.com>
2026-05-14 14:24:02 -0300

    agrego pagina principal con diseño

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const path = require('path');
 const pool = require('./db/connection');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.json({ mensaje: 'Servidor funcionando' });
 });
 
